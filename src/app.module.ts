313dde768e41c5badedff8c185fdcc0a3f97f6f7
Nicoly Cunha <nicolyoliveiracunha.2001@gmail.com>
2025-10-25 19:55:49 -0300

    feat: add authenticate and create question route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from 'src/auth/auth.module'
+import { AuthenticateController } from 'src/controllers/authenticate.controller'
 import { CreateAccountController } from 'src/controllers/create-account.controller'
+import { CreateQuestionController } from 'src/controllers/create-question.controller'
 import { envSchema } from 'src/env'
 import { PrismaService } from 'src/prisma/prisma.service'
 
@@ -9,9 +12,14 @@ import { PrismaService } from 'src/prisma/prisma.service'
     ConfigModule.forRoot({
       validate: env => envSchema.parse(env),
       isGlobal: true
-    })
+    }),
+    AuthModule
   ],
-  controllers: [CreateAccountController],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionController
+  ],
   providers: [PrismaService]
 })
 export class AppModule {}
